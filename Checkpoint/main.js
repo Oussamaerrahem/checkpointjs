a9d74d0219e4671d795befbfc30e4c1331ba918a
Oussamaerrahem <56201426+Oussamaerrahem@users.noreply.github.com>
2019-11-09 15:13:54 +0100

    Add files via upload

---
+++ Checkpoint/main.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    document.getElementById("area").style.fontWeight = 'bold';
+  }
+  function myFunction2() {
+    document.getElementById('area').style.fontStyle = 'italic';
+
+  }
+  function myFunction3() {
+     document.getElementById('area').style.textDecoration = 'underline';
+
+  }
+
+  function changetaille(){
+    
+    document.getElementById('area').style.fontSize =document.getElementById('taille').value;
+  }
+  function changefont() {
+document.getElementById('area').style.fontFamily = document.getElementById('font').value;
+
+  }
